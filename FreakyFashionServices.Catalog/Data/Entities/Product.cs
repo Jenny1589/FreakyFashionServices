d6a808d9f3c850138051b6c0e67cdaeef8aa2c03
Jenny <jenny.kallerup@outlook.com>
2020-12-28 16:18:27 +0100

    Add product entity

---
+++ FreakyFashionServices.Catalog/Data/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FreakyFashionServices.Catalog.Data.Entities
+{
+    public class Product
+    {
+        public Product(Guid id, string name, string description, double price, int availableStock)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            AvailableStock = availableStock;
+        }
+
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public double Price { get; protected set; }
+        public int AvailableStock { get; protected set; }
+    }
+}
