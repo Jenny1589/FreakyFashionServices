0387a457332a7ab182ac90e9bbe9ca22b447abab
Jenny <jenny.kallerup@outlook.com>
2020-12-28 16:19:46 +0100

    Create Product table
    Add entity configurations to DbContext
    Add Db migrations

--- FreakyFashionServices.Catalog/Data/FreakyFashionCatalogDbContext.cs
+++ FreakyFashionServices.Catalog/Data/FreakyFashionCatalogDbContext.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FreakyFashionServices.Catalog.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FreakyFashionServices.Catalog.Data
 {
     public class FreakyFashionCatalogDbContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
         public FreakyFashionCatalogDbContext(DbContextOptions<FreakyFashionCatalogDbContext> options)
             : base(options) 
         { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .ToTable("Product")
+                .Property(p => p.Id)
+                .ValueGeneratedNever();
+        }
     }
 }
