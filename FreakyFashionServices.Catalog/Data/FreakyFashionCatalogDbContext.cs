3ed29aceafa4cb0605f6ab7cd6ed61c02d7484b0
Jenny <jenny.kallerup@outlook.com>
2020-12-28 15:51:43 +0100

    Add EF Core to project
    Add EFCore NuGet packages
    Add Connection string in appsettings.json
    Add an application DbContext and use in Startup
    Remove uneccessary using statements

---
+++ FreakyFashionServices.Catalog/Data/FreakyFashionCatalogDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FreakyFashionServices.Catalog.Data
+{
+    public class FreakyFashionCatalogDbContext : DbContext
+    {
+        public FreakyFashionCatalogDbContext(DbContextOptions<FreakyFashionCatalogDbContext> options)
+            : base(options) 
+        { }
+    }
+}
--- FreakyFashionServices.Catalog/Startup.cs
+++ FreakyFashionServices.Catalog/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FreakyFashionServices.Catalog.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace FreakyFashionServices.Catalog
 {
@@ -26,6 +21,8 @@ namespace FreakyFashionServices.Catalog
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<FreakyFashionCatalogDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
