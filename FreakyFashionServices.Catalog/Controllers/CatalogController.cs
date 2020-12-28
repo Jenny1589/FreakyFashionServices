b2d2d6f62018ed3274329fd16cb9e6e2a7ffd8d1
Jenny <jenny.kallerup@outlook.com>
2020-12-28 21:23:13 +0100

    Add CatalogController with GET methods
    Add ProductDto
    Add method to get all products from the catalog.
    Add method fto get one product from the catalog based on id

---
+++ FreakyFashionServices.Catalog/Controllers/CatalogController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FreakyFashionServices.Catalog.Data;
+using FreakyFashionServices.Catalog.Data.Dtos;
+using FreakyFashionServices.Catalog.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreakyFashionServices.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+
+        private FreakyFashionCatalogDbContext _context;
+
+        public CatalogController(FreakyFashionCatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("items")]
+        public IEnumerable<ProductDto> GetItems()
+        {
+            return _context.Products.Select(p => ConvertToDto(p));
+        }
+
+        [HttpGet("items/{id}")]
+        public ActionResult GetItem(Guid id)
+        {
+            var foundProduct = _context.Products.FirstOrDefault(p => p.Id.Equals(id));
+
+            if (Object.Equals(foundProduct, null)) return NotFound();
+
+            return Ok(ConvertToDto(foundProduct));
+        }
+
+        private static ProductDto ConvertToDto(Product product)
+        {
+            return new ProductDto
+            {
+                Id = product.Id.ToString(),
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                AvailableStock = product.AvailableStock
+            };
+        }
+    }
+}
---
+++ FreakyFashionServices.Catalog/Data/Dtos/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FreakyFashionServices.Catalog.Data.Dtos
+{
+    public class ProductDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int AvailableStock { get; set; }
+    }
+}
