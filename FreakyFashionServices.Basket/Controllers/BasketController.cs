978a1cd72c9555db95231d5201ddd6d650565d3e
Jenny <jenny.kallerup@outlook.com>
2021-01-04 22:19:49 +0100

    Add API basket controller
    Add model for basket item
    Add model for a shopping basket
    Add basket API controller
    Add API PUT method for updating a basket
    Add API GET method to fetch a basket

---
+++ FreakyFashionServices.Basket/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using FreakyFashionServices.Basket.Extensions;
+using FreakyFashionServices.Basket.Models;
+
+namespace FreakyFashionServices.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private IDistributedCache _cache;
+
+        public BasketController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateBasketAsync(string id, [FromBody]ShoppingBasket basket)
+        {
+            await _cache.SetRecordAsync<ShoppingBasket>(id, basket);
+
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetBasketAsync(string id)
+        {
+            var basket = await _cache.GetRecordAsync<ShoppingBasket>(id);
+
+            if (basket is null) return NotFound();
+
+            return Ok(basket);
+        }
+    }
+}
---
+++ FreakyFashionServices.Basket/Models/ShoppingBasket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace FreakyFashionServices.Basket.Models
+{
+    public class ShoppingBasket
+    {
+        public IEnumerable<BasketItem> Items { get; set; }
+
+        public class BasketItem
+        {
+            public string Id { get; set; }
+            public string ProductName { get; set; }
+            public double UnitPrice { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
