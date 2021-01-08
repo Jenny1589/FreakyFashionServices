bfbb55e9611af14bfc8e127f2b471ec1251d400c
Jenny <jenny.kallerup@outlook.com>
2021-01-08 10:00:02 +0100

    Add console app with ability to process orders
    Configure RabbitMQ connection
    Set up application as a consumer

---
+++ FreakyFashionServices.OrderConsole/Data/Entities/Order.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace FreakyFashionServices.OrderConsole.Data.Entities
+{
+    public class Order
+    {
+        [JsonConstructor]
+        public Order(string customerIdentifier, IEnumerable<OrderItem> items)
+        {
+            CustomerIdentifier = customerIdentifier;
+            Items = items;
+        }
+
+        public int OrderId { get; protected set; }
+        public string CustomerIdentifier { get; protected set; }
+        public IEnumerable<OrderItem> Items { get; protected set; }
+
+        public class OrderItem {
+
+            public OrderItem(string id, string productName, double unitPrice, int quantity)
+            {
+                Id = id;
+                ProductName = productName;
+                UnitPrice = unitPrice;
+                Quantity = quantity;
+            }
+
+            public string Id { get; protected set; }
+            public string ProductName { get; protected set; }
+            public double UnitPrice { get; protected set; }
+            public int Quantity { get; protected set; }
+        }
+    }
+}
---
+++ FreakyFashionServices.OrderConsole/Program.cs
@@ -0,0 +1,72 @@
+﻿using FreakyFashionServices.OrderConsole.Data.Entities;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace FreakyFashionServices.OrderConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var isRunning = true;
+            Console.WriteLine("Waiting for orders...");
+
+            using var connection = CreateRabbitMqConnection();
+            using var channel = connection.CreateModel();
+
+            var queueName = "order";
+            DeclareMessageQueue(channel, queueName);
+
+            var consumer = InitializeRabbitMqConsumer(channel, queueName);
+            consumer.Received += OnRecieved;
+
+            Console.ReadKey(false);
+        }
+
+        private static IConnection CreateRabbitMqConnection()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 15672
+            };
+
+            return factory.CreateConnection();
+        }
+
+        private static void DeclareMessageQueue(IModel channel, string queueName)
+        {
+            channel.QueueDeclare(queue: queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+        }
+
+        private static EventingBasicConsumer InitializeRabbitMqConsumer(IModel channel, string queueName)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(queue: queueName,
+                autoAck: true,
+                consumer: consumer);
+
+            return consumer;
+        }
+
+        private static void OnRecieved(object model, BasicDeliverEventArgs ea)
+        {
+            var encodedOrder = ea.Body.ToArray();
+            var serializedOrder = Encoding.UTF8.GetString(encodedOrder);
+
+            var order = JsonConvert.DeserializeObject<Order>(serializedOrder);
+
+            Console.WriteLine($"{order.CustomerIdentifier}");
+
+            //Save incoming order in database.
+        }
+    }
+}
